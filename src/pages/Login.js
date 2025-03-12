4439fee1c0f19d5a4e69d180c94ee4c6e93ae9c2
madsensss <madsenss@naver.com>
2025-03-12 15:44:01 +0900

    a

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -104,7 +104,7 @@ const Login = () => {
     };
 
     const handleGoogleLogin = () => {
-        window.location.href = "https://mentoview.site/oauth2/authorization/google";
+        window.location.href = "https://mentoview.site/api/oauth2/authorization/google";
     };
 
     return (
